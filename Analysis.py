670e58cbc0b7cea162a8ed952ef44b911665f539
rnibhriain <78806345+rnibhriain@users.noreply.github.com>
2024-01-01 19:53:19 +0000

    Create Analysis.py

---
+++ Analysis.py
@@ -0,0 +1,18 @@
+# Risk Levels
+# 0 - Not risky < 7 days
+# 1 - Risky > 7 days
+
+def predictRisk ( package ):
+    
+    # default is 0
+    return 0
+
+def main():
+    print( "Please enter a package you would like to use from our list of packages" )
+    ######################################################
+    # come up with set list of packages
+    ######################################################
+    ##  https://services.nvd.nist.gov/rest/json/cves/2.0
+
+if __name__ == "__main__":
+    main()
