e0a15d1e4e6f7f0d20f92e64f11d0817791840ae
rnibhriain <78806345+rnibhriain@users.noreply.github.com>
2024-01-01 23:12:08 +0000

    Json begun

--- Analysis.py
+++ Analysis.py
@@ -1,3 +1,6 @@
+import requests
+import json
+
 # Risk Levels
 # 0 - Not risky < 7 days
 # 1 - Risky > 7 days
@@ -7,12 +10,20 @@ def predictRisk ( package ):
     # default is 0
     return 0
 
+def print_json ( object ):
+    
+    text = json.dumps( object, sort_keys = True, indent = 4 )
+    print( text )
+
 def main():
     print( "Please enter a package you would like to use from our list of packages" )
     ######################################################
     # come up with set list of packages
     ######################################################
-    ##  https://services.nvd.nist.gov/rest/json/cves/2.0
+    ##  
+    response = requests.get( "https://services.nvd.nist.gov/rest/json/cves/2.0" )
+    # print( response.json() )
+    print_json( response.json()['description'] )
 
 if __name__ == "__main__":
     main()
