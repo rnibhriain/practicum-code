34f5289ecdf700510eb157b36aa61aa9e55b5d8e
rnibhriain <78806345+rnibhriain@users.noreply.github.com>
2024-01-02 18:59:12 +0000

    some dates secured

--- Analysis.py
+++ Analysis.py
@@ -10,23 +10,38 @@ def predictRisk ( package ):
     # default is 0
     return 0
 
+class VulDates:
+    def __init__( self, id, pub, mod ):
+        self.id = id
+        self.pub = pub
+        self.mod = mod
+
+def find_published_dates ( object ):
+    dateobj = VulDates( object['id'], object['published'], object['lastModified'] )
+    return dateobj
+
 def print_json ( object ):
     
     text = json.dumps( object, sort_keys = True, indent = 4 )
     print( text )
-
+    
+ 
 def main():
     print( "Please enter a package you would like to use from our list of packages" )
+
     ######################################################
     # come up with set list of packages
+    # for now lets focus on apache cus idk
     ######################################################
-    ## https://cve.circl.lu/api/search/microsoft/office
-    ## https://services.nvd.nist.gov/rest/json/cves/2.0?cvssV2Severity=LOW
-    ## http://cve.circl.lu/api/search/apache/maven
+    
     response = requests.get( "https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=apache maven " )
     print( response.status_code )
-    print_json( response.json()['vulnerabilities'][0]['cve']['published'] )
-    print_json( response.json()['vulnerabilities'][0]['cve']['descriptions'] )
+    print_json( response.json()['vulnerabilities'] )
+
+    for i in response.json()['vulnerabilities']:
+        find_published_dates( i['cve'] )
+    # print_json( response.json()['vulnerabilities'][0]['cve']['published'] )
+    # print_json( response.json()['vulnerabilities'][0]['cve']['descriptions'] )
     
 
 if __name__ == "__main__":
