6a5ad1ed8f29782ee106adfa0941b6b35447f979
rnibhriain <78806345+rnibhriain@users.noreply.github.com>
2024-01-02 18:11:25 +0000

    lets focus on apache 4 now

--- Analysis.py
+++ Analysis.py
@@ -20,10 +20,14 @@ def main():
     ######################################################
     # come up with set list of packages
     ######################################################
-    ##  
-    response = requests.get( "https://services.nvd.nist.gov/rest/json/cves/2.0" )
-    # print( response.json() )
-    print_json( response.json()['description'] )
+    ## https://cve.circl.lu/api/search/microsoft/office
+    ## https://services.nvd.nist.gov/rest/json/cves/2.0?cvssV2Severity=LOW
+    ## http://cve.circl.lu/api/search/apache/maven
+    response = requests.get( "https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=apache maven " )
+    print( response.status_code )
+    print_json( response.json()['vulnerabilities'][0]['cve']['published'] )
+    print_json( response.json()['vulnerabilities'][0]['cve']['descriptions'] )
+    
 
 if __name__ == "__main__":
     main()
