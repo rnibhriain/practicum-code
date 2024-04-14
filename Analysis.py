54e3ee60a502740c82a9d52d4c70322e77810953
rnibhriain <78806345+rnibhriain@users.noreply.github.com>
2024-04-14 20:27:39 +0100

    Doing some organising - should probs split these again

--- Analysis.py
+++ Analysis.py
@@ -5,10 +5,44 @@ import datetime
 # Risk Levels
 # 0 - Not risky < 7 days
 # 1 - Risky > 7 days
-
 def predictRisk ( package ):
     
     # default is 0
+    return 0
+
+###############################################################################
+# Vulnerability Prediction by Code Metrics                                    #
+###############################################################################
+def codePrediction ():
+    
+
+
+    return 0
+###############################################################################
+
+###############################################################################
+# Vulnerability Prediction by Project Metrics                                 #
+###############################################################################
+def projectPrediction ():
+    
+
+
+    return 0
+###############################################################################
+
+###############################################################################
+# Vulnerability Prediction by NVD Data                                        #
+###############################################################################
+def vulPrediction ():
+    response = requests.get( "https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=apache maven " )
+    print( response.status_code )
+    # print_json( response.json()['vulnerabilities'] )
+
+    for i in response.json()['vulnerabilities']:
+        find_published_dates( i['cve'] )
+    # print_json( response.json()['vulnerabilities'][0]['cve']['published'] )
+    # print_json( response.json()['vulnerabilities'][0]['cve']['descriptions'] )
+
     return 0
 
 class VulDates:
@@ -18,6 +52,7 @@ class VulDates:
         self.mod = mod
 
 def find_published_dates ( object ):
+    
     dateobj = VulDates( object[ 'id' ], object[ 'published' ], object[ 'lastModified' ] )
     print( object[ 'id' ] )
     print( datetime.strptime( object[ 'published' ] ) )
@@ -28,6 +63,8 @@ def print_json ( object ):
     
     text = json.dumps( object, sort_keys = True, indent = 4 )
     print( text )
+
+###############################################################################
     
  
 def main():
@@ -37,15 +74,6 @@ def main():
     # come up with set list of packages
     # for now lets focus on apache cus idk
     ######################################################
-    
-    response = requests.get( "https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=apache maven " )
-    print( response.status_code )
-    # print_json( response.json()['vulnerabilities'] )
-
-    for i in response.json()['vulnerabilities']:
-        find_published_dates( i['cve'] )
-    # print_json( response.json()['vulnerabilities'][0]['cve']['published'] )
-    # print_json( response.json()['vulnerabilities'][0]['cve']['descriptions'] )
     
 
 if __name__ == "__main__":
